50c2739d804a34e3e78de9b478bfc2d1023aeba2
AniketR9 <rajeshaniket13@gmail.com>
2024-07-26 17:06:43 +0530

    Updated register.jsx

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
@@ -6,6 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const app=express();
 
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 
--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -1,17 +1,41 @@
 import "./register.scss";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import axios from "axios";
 
-function Register(){
-    return (
-        <div className="registerPage">
+function Register() {
+
+    const [error,setError]=useState("");
+    const navigate=useNavigate();
+
+    const handleSubmit= async (e)=>{
+        e.preventDefault();
+        const formData=new FormData(e.target);
+
+        const username=formData.get("username");
+        const email=formData.get("email");
+        const password=formData.get("password");
+        try{
+            const res=await axios.post("http://localhost:8800/api/auth/register",{
+             username,email,password
+            });
+            navigate("/login");
+        }catch(err){
+            // console.log(err);
+            setError(err.response.data.message);
+        }
+    };
+
+  return (
+    <div className="registerPage">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
-          <button >Register</button>
+          <button>Register</button>
+          {error && <span>{error}</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
       </div>
@@ -19,7 +43,7 @@ function Register(){
         <img src="/bg.png" alt="" />
       </div>
     </div>
-    );
-};
+  );
+}
 
 export default Register;
