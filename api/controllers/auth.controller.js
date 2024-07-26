77b45a9b141783b06b764b162fce5c56ac7d9c26
AniketR9 <rajeshaniket13@gmail.com>
2024-07-26 11:26:30 +0530

    Added Prisma

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+import bcrypt from "bcrypt";
+
+export const register=async (req,res)=>{
+    const {username,email,password}=req.body;
+    // db operations
+    // console.log(req.body);
+    // Hash the password
+    const hashedPassword= await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+    // Create a new user and save to db
+}
+
+export const login=(req,res)=>{
+    // db operations
+}
+
+export const logout=(req,res)=>{
+    // db operations
+}
