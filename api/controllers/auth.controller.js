5d05ccc53b0998df525df6b72d766d3d4c1dbfe8
AniketR9 <rajeshaniket13@gmail.com>
2024-07-26 12:53:27 +0530

    Added user schema and register functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,28 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 
 export const register=async (req,res)=>{
     const {username,email,password}=req.body;
     // db operations
     // console.log(req.body);
     // Hash the password
+    try{
     const hashedPassword= await bcrypt.hash(password, 10);
     console.log(hashedPassword);
     // Create a new user and save to db
+    const newUser = await prisma.user.create({
+        data: {
+          username,
+          email,
+          password: hashedPassword,
+        },
+      });
+      console.log(newUser);
+      res.status(201).json({ message: "User created successfully" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Failed to create user! " });
+    }
 }
 
 export const login=(req,res)=>{
