6c336483ad8048148aa34bb6a2e3d7b2f7ff385a
AniketR9 <rajeshaniket13@gmail.com>
2024-07-26 16:12:26 +0530

    Update login.jsx

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,8 +1,36 @@
-import './login.scss';
+import { useContext, useState } from "react";
+import "./login.scss";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login(){
     return (
-        <div className="login">Login</div>
+        <div className="login">
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <h1>Welcome back</h1>
+          <input
+            name="username"
+            required
+            minLength={3}
+            maxLength={20}
+            type="text"
+            placeholder="Username"
+          />
+          <input
+            name="password"
+            type="password"
+            required
+            placeholder="Password"
+          />
+          <button disabled={isLoading}>Login</button>
+          {error && <span>{error}</span>}
+          <Link to="/register">{"Don't"} you have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
     )
 }
 
