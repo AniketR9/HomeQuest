b6484b052042df4dc4396af0b759caf291c8ff8c
AniketR9 <rajeshaniket13@gmail.com>
2024-07-29 10:38:08 +0530

    Added secure routes

--- api/controllers/test.controller.js
+++ api/controllers/test.controller.js
@@ -1,16 +1,9 @@
 import jwt from "jsonwebtoken";
 
 export const shouldBeLoggedIn= async(req,res)=>{
-    const token=req.cookies.token;
-
-    if(!token)return res.status(401).json({message:" Not Authenticated"});
-
-    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err,payload)=>{
-        if(err) return res.status(403).json({message:"Token is not valid!"});
-    });
-
+    console.log(req.userId);
     res.status(200).json({message:"You are authenticated!"});
-}
+};
 
 export const shouldBeAdmin= async(req,res)=>{
     const token=req.cookies.token;
@@ -25,4 +18,4 @@ export const shouldBeAdmin= async(req,res)=>{
     });
 
     res.status(200).json({message:"You are authenticated!"});
-}
+};
