bdb394229374f750e93a0197ebdd37181feb224d
AniketR9 <rajeshaniket13@gmail.com>
2024-07-29 15:31:34 +0530

    Updation portion is done

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -45,8 +45,8 @@ export const updateUser = async (req, res) => {
         ...(avatar && { avatar }),
       },
     });
-
-    res.status(200).json(updatedUser);
+    const { password: userPassword, ...rest } = updatedUser;
+    res.status(200).json(rest);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to update users" });
--- client/src/routes/profileUpdatePage/profileUpdatePage.jsx
+++ client/src/routes/profileUpdatePage/profileUpdatePage.jsx
@@ -1,21 +1,47 @@
-import React, { useContext } from 'react';
-import {AuthContext} from "../../context/AuthContext";
-import './profileUpdatePage.scss';
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import "./profileUpdatePage.scss";
+import {useNavigate} from "react-router-dom";
+import apiRequest from "../../lib/apiRequest.js";
 
 function ProfileUpdatePage() {
+  const [error, setError] = useState("");
+  const { currentUser, updateUser } = useContext(AuthContext);
 
-    const {currentUser,updateUser}=useContext(AuthContext);
+  const navigate= useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const { username, email, password } = Object.fromEntries(formData);
+
+    try {
+      const res = await apiRequest.put(`/user/${currentUser.id}`, {
+        username,
+        email,
+        password,
+      });
+      updateUser(res.data);
+      navigate("/profile");
+    } catch (error) {
+      console.log(error);
+      setError(err.response.data.message);
+    }
+  };
+
   return (
-    <div className='profileUpdatePage'>
+    <div className="profileUpdatePage">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Update Profile</h1>
           <div className="item">
             <label htmlFor="username">Username</label>
             <input
               id="username"
               name="username"
-              type="text" defaultValue={currentUser.username}
+              type="text"
+              defaultValue={currentUser.username}
             />
           </div>
           <div className="item">
@@ -32,13 +58,18 @@ function ProfileUpdatePage() {
             <input id="password" name="password" type="password" />
           </div>
           <button>Update</button>
+          {error && <span>error</span>}
         </form>
       </div>
       <div className="sideContainer">
-        <img src={currentUser.avatar || "/noavatar.jpg"} alt="" className="avatar" />
+        <img
+          src={currentUser.avatar || "/noavatar.jpg"}
+          alt=""
+          className="avatar"
+        />
       </div>
     </div>
-  )
+  );
 }
 
 export default ProfileUpdatePage;
