7c59ba4f8ef891bc9cd5d5d3d83e2d78b503fa94
AniketR9 <rajeshaniket13@gmail.com>
2024-07-30 08:49:36 +0530

    Updated the singlepage

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -5,7 +5,7 @@ import "leaflet/dist/leaflet.css";
 
 function Map({items}){
     return(
-    <MapContainer center={[51.50,-0.12]} zoom={7} scrollWheelZoom={false} className='map'>
+    <MapContainer center={items.length==1 ? [items[0].latitude,items[0].longitude] : [21.7679,78.8718]} zoom={7} scrollWheelZoom={false} className='map'>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
