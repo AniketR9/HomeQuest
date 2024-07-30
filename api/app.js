224126e66f26fe23a7fa5dac2fff63497135c75c
AniketR9 <rajeshaniket13@gmail.com>
2024-07-30 20:37:55 +0530

    Added chat functionality

--- api/app.js
+++ api/app.js
@@ -4,6 +4,8 @@ import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 
@@ -19,6 +21,8 @@ app.use("/api/auth",authRoute);
 app.use("/api/test",testRoute);
 app.use("/api/user",userRoute);
 app.use("/api/post",postRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 // app.use("/test-api/test",testRoute);
 
 app.listen(8800,()=>{
